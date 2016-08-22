c6bb3e36050c3f889525c77614eb56d012ae3a09
shri <shrimeister@gmail.com>
2016-08-23 07:34:37 +0800

    file for modifying sauce labs hosts file in windows.

---
+++ ModifyHosts.bat
@@ -0,0 +1,4 @@
+@echo off
+echo 10.211.233.222 cert.tse.visa.com > %temp%\temphosts.txt
+type C:\WINDOWS\system32\drivers\etc\hosts >> %temp%\temphosts.txt
+copy /Y %temp%\temphosts.txt C:\WINDOWS\system32\drivers\etc\hosts
