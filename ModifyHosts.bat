750c37c4d8d261ce4bf2626ce383d77675bf9dc1
shri <shrimeister@gmail.com>
2016-08-23 13:30:34 +0800

    Delete ModifyHosts.bat
    delete this file, since the issue is handled in sauce connect

--- ModifyHosts.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo 10.211.233.222 cert.tse.visa.com > %temp%\temphosts.txt
-type C:\WINDOWS\system32\drivers\etc\hosts >> %temp%\temphosts.txt
-copy /Y %temp%\temphosts.txt C:\WINDOWS\system32\drivers\etc\hosts
